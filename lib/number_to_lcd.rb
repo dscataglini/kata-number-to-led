8c1e42544c0d8a783887acfb0e440f3f87016198
Diego Scataglini <dscataglini@diegomac.local>
2009-11-03 16:30:03 -0500

    normalized the samples

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -7,8 +7,21 @@ class NumberToLcd
   class LCDNumber
 		class << self
 	    def convert(number)
-				File.open( File.expand_path(File.dirname(__FILE__) + "/samples/#{number}.txt" ), "r").read
+				numbers = to_string_array(number)
+				lcd_numbers = numbers.map do |digit| 
+					digit_reppresentation(digit)
+				end
+				return lcd_numbers.join.chop if number < 10
 	    end
+	
+			private
+			def to_string_array(number)
+				number.to_s.split("")
+			end
+			
+			def digit_reppresentation(digit)
+				File.open( File.expand_path(File.dirname(__FILE__) + "/samples/#{digit}.txt" ), "r").read.lines.to_a
+			end
 		end
     
   end
