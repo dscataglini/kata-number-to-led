cbdb497f9412c648d45b357d002db864a051a607
Diego Scataglini <dscataglini@diegomac.local>
2009-11-03 16:06:53 -0500

    covering the first 10 cases in solution #2

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -5,31 +5,9 @@ class NumberToLcd
   end
   
   class LCDNumber
-    PATTERNS = {:none => "   ", :right => "  |",  :middle => " - ", :sides => "| |", :left => "|  "}
-    PROTOS = {"1" => [:none,    :right,   :none,    :right,   :none], 
-              "2" => [:middle,  :right,   :middle,  :left,    :middle],
-              "3" => [:middle,  :right,   :middle,  :right,   :middle],
-              "4" => [:none,    :sides,   :middle,  :right,   :none],
-              "5" => [:middle,  :left,    :middle,  :right,   :middle],
-              "6" => [:middle,  :left,    :middle,  :sides,   :middle],
-              "7" => [:middle,  :right,   :none,    :right,   :none],
-              "8" => [:middle,  :sides,   :middle,  :sides,   :middle],
-              "9" => [:middle,  :sides,   :middle,  :right,   :middle],
-              "0" => [:middle,  :sides,   :none,    :sides,   :middle]
-              }
 		class << self
 	    def convert(number)
-	      protos = number.to_s.chars.to_a.map{|num| PROTOS[num] }
-	      number_definitions_in_order = protos.shift.zip(*protos)
-				number_definitions_in_order.inject(""){ |str, row| str << protos_to_pattern( *row ) }.chop
-	    end
-    
-	    private
-    
-	    def protos_to_pattern(*protos)
-	      protos.map do |el| 
-	        PATTERNS[el]
-	      end.join(" ") << "\n"
+				File.open( File.expand_path(File.dirname(__FILE__) + "/samples/#{number}.txt" ), "r").read
 	    end
 		end
     
