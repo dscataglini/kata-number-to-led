7cd2bf2d72197dca3f00c2f46eac7a0c56fb7292
Diego Scataglini <dscataglini@diegomac.local>
2009-11-04 00:55:23 -0500

    extracted a method

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -11,15 +11,19 @@ class NumberToLcd
     digits = number_to_digits( this_number )
     lcd_number = ""
     5.times do |row|
-      lcd_number << digits.map do |digit|
-         LCD_NUMBERS[row][digit.to_i]
-      end.join(" ") + "\n"
+      lcd_number << compose_row(row, digits)
     end
     lcd_number.chop
   end
   
   private 
+  def self.compose_row(row, digits)
+    digits.map do |digit|
+       LCD_NUMBERS[row][digit]
+    end.join(" ") + "\n"
+  end
+  
   def self.number_to_digits( number )
-    number.to_s.split("")
+    number.to_s.split("").map{|digit| digit.to_i}
   end
 end
