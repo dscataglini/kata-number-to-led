381811ab6952b48e5dcd5504a65fa91a1f50c946
Diego Scataglini <dscataglini@diegomac.local>
2009-11-04 00:32:16 -0500

    my third solution to the number to lcd code kata

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -1,4 +1,25 @@
 class NumberToLcd
-  def self.convert(this_number)
+  LCD_NUMBERS = [
+       [" - ","   "," - "," - ","   "," - "," - "," - "," - "," - "],
+       ["| |","  |","  |","  |","| |","|  ","|  ","  |","| |","| |"],
+       ["   ","   "," - "," - "," - "," - "," - ","   "," - "," - "],
+       ["| |","  |","|  ","  |","  |","  |","| |","  |","| |","  |"],
+       [" - ","   "," - "," - ","   "," - "," - ","   "," - "," - "],
+      ]
+    
+  def self.convert( this_number )
+    digits = number_to_digits( this_number )
+    lcd_number = ""
+    5.times do |row|
+      lcd_number << digits.map do |digit|
+         LCD_NUMBERS[row][digit.to_i]
+      end.join(" ") + "\n"
+    end
+    lcd_number.chop
+  end
+  
+  private 
+  def self.number_to_digits( number )
+    number.to_s.split("")
   end
 end
