1602421d78bea459b923194f839b4dddb782e469
Diego Scataglini <dscataglini@diegomac.local>
2009-11-04 00:51:41 -0500

    a variation on solution #3

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -1,4 +1,38 @@
 class NumberToLcd
-  def self.convert(this_number)
+  LCD_NUMBERS = [
+      [" - ", "| |", "   ", "| |", " - "],
+      ["   ", "  |", "   ", "  |", "   "],
+      [" - ", "  |", " - ", "|  ", " - "],
+      [" - ", "  |", " - ", "  |", " - "],
+      ["   ", "| |", " - ", "  |", "   "],
+      [" - ", "|  ", " - ", "  |", " - "],
+      [" - ", "|  ", " - ", "| |", " - "],
+      [" - ", "  |", "   ", "  |", "   "],
+      [" - ", "| |", " - ", "| |", " - "],
+      [" - ", "| |", " - ", "  |", " - "]
+    ]
+    
+  class << self
+    def convert( number )
+      lcd_numbers = fetch_chars( number )
+      lcd_numbers.map{|row| row.join(" ") << "\n"}.to_s.chop
+    end
+  
+    private 
+    def number_to_digits( number )
+      number.to_s.split("")
+    end
+    
+    def fetch_chars( number )
+      lcd_numbers = Array.new( 5 ){[]}
+      digits = number_to_digits( number )
+      digits.each do |digit|
+        5.times do |row|
+          lcd_numbers[row] << LCD_NUMBERS[digit.to_i][row]
+        end
+      end
+      lcd_numbers
+    end
   end
+  
 end
